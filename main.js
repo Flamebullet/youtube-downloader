4bcf81019a4db79668d5e13c18c3475aff907035
Flame <battle4galaxy@gmail.com>
2021-12-21 20:11:32 +0800

    v1.0.0

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) return app.quit();
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		autoHideMenuBar: true,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js')
+		}
+	});
+
+	// win.setMenuBarVisibility(false);
+	win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit();
+});
